f75814824291bff33db42968ce84c22b1e161def
Mohammadsalman19 <mohammadsalman1999.9.5@gmail.com>
2024-08-24 16:36:48 +0530

    Added Singleton to DB

--- app/src/main/java/com/example/roomdb/database/ContactDB.kt
+++ app/src/main/java/com/example/roomdb/database/ContactDB.kt
@@ -1,11 +1,32 @@
 package com.example.roomdb.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.roomdb.daos.ContactDAO
 import com.example.roomdb.entities.Contact
 
 @Database(entities = [Contact::class], version = 1)
-abstract class ContactDB: RoomDatabase() {
-    abstract fun getContactDAO () : ContactDAO
+abstract class ContactDB : RoomDatabase() {
+    abstract fun getContactDAO(): ContactDAO
+
+    companion object {
+
+        @Volatile
+        private var instance: ContactDB? = null
+
+        @Synchronized
+        fun getInstance(context: Context): ContactDB {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ContactDB::class.java,
+                    "Contact_database"
+                ).build()
+            }
+
+            return instance!!
+        }
+    }
 }
