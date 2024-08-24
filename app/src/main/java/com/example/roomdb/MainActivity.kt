544c7f1fd08decd04b459372f77608c42bf99e5a
Mohammadsalman19 <mohammadsalman1999.9.5@gmail.com>
2024-08-24 18:03:34 +0530

    created edit text and buttons and assigned to xml views

--- app/src/main/java/com/example/roomdb/MainActivity.kt
+++ app/src/main/java/com/example/roomdb/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.example.roomdb
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.roomdb.database.ContactDB
+import com.example.roomdb.entities.Contact
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var textId: EditText
+    private lateinit var textName: EditText
+    private lateinit var textPhone: EditText
+
+    private lateinit var insert: Button
+    private lateinit var update: Button
+    private lateinit var delete: Button
+    private lateinit var read: Button
+
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        textId = findViewById(R.id.editTextId)
+        textName = findViewById(R.id.editTextName)
+        textPhone = findViewById(R.id.editTextPhone)
+
+        insert = findViewById(R.id.buttonInsert)
+        update = findViewById(R.id.buttonUpdate)
+        delete = findViewById(R.id.buttonDelete)
+        read = findViewById(R.id.buttonRead)
+
     }
 }
